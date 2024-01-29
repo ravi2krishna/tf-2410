0de360a8ccd6740ad72596ad1216a28e2cf968ba
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:22:53 +0530

    create web route table

--- vpc.tf
+++ vpc.tf
@@ -40,3 +40,18 @@ resource "aws_internet_gateway" "myntra-igw" {
     Name = "myntra-internet-gateway"
   }
 }
+
+# Web Route Table
+resource "aws_route_table" "myntra-web-rt" {
+  vpc_id = aws_vpc.myntra-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myntra-igw.id
+  }
+
+  tags = {
+    Name = "myntra-web-route-table"
+  }
+}
+
