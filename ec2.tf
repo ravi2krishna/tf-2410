085278536ad457fcdf1206b6180067892adfe840
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:59:13 +0530

    pass sg in list

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name = "ravi-oero"
   subnet_id     = aws_subnet.ibm-web-sn.id
-  vpc_security_group_ids = aws_security_group.ibm-web-sg.id
+  vpc_security_group_ids = [aws_security_group.ibm-web-sg.id]
   user_data = file("ecomm.sh")
 
   tags = {
