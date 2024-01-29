edabfa868de2fc661da3d8772b2df1f720ebaf80
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:40:06 +0530

    create web security group

--- vpc.tf
+++ vpc.tf
@@ -142,3 +142,34 @@ resource "aws_network_acl_association" "myntra-db-nacl-asc" {
   subnet_id      = aws_subnet.myntra-db-sn.id
 }
 
+# Web Security Group
+resource "aws_security_group" "myntra-web-sg" {
+  name        = "myntra-web-traffic"
+  description = "Allow SSH - HTTP inbound traffic"
+  vpc_id      = aws_vpc.myntra-vpc.id
+
+  ingress {
+    description = "SSH from WWW"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from WWW"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "myntra-web-sg"
+  }
+}
