dfc789b34db938055510529ec5b16657035c0188
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:17:22 +0530

    create database subnet

--- vpc.tf
+++ vpc.tf
@@ -19,3 +19,15 @@ resource "aws_subnet" "myntra-web-sn" {
     Name = "myntra-web-subnet"
   }
 }
+
+# Database Subnet
+resource "aws_subnet" "myntra-db-sn" {
+  vpc_id     = aws_vpc.myntra-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "myntra-database-subnet"
+  }
+}
