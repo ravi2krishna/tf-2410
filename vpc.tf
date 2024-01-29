6c536fdf72a48702d326efb942ae4c0959d2728e
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:14:23 +0530

    create web subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "myntra-vpc" {
     Name = "myntra"
   }
 }
+
+# Web Subnet
+resource "aws_subnet" "myntra-web-sn" {
+  vpc_id     = aws_vpc.myntra-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "myntra-web-subnet"
+  }
+}
