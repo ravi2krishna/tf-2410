33600f9fb7558b86a55b69cb571d2b6ca5101e93
ravi2krishna <ravi2krishna@gmail.com>
2024-01-29 10:56:42 +0530

    create ec2 server

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+# Web Server
+resource "aws_instance" "example" {
+  ami           = "ami-08e2c1a8d17c2fe17"
+  instance_type = "t2.micro"
+  key_name = "ravi-oero"
+  subnet_id     = aws_subnet.ibm-web-sn.id
+  vpc_security_group_ids = aws_security_group.ibm-web-sg.id
+  user_data = file("ecomm.sh")
+
+  tags = {
+    Name = "ibm-web-server"
+  }
+
+}
